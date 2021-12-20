59f8f942ab8c7335ca68b738800ae5b748428558
Oleg Raytsin <o.raytsin@adignos.de>
2021-12-20 16:18:59 +0100

    Solved tasks from module 6

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,41 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.render();
+  }
+
+  render() {
+    let table = document.createElement('TABLE');
+    let tableHeader = document.createElement('THEAD');
+
+    tableHeader.innerHTML = `
+      <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+      </tr> 
+      `;
+
+    table.append(tableHeader);
+
+    let tableBody = document.createElement('TBODY');
+    tableBody.innerHTML = this.rows
+      .map(row => `<tr><td>${row.name}</td><td>${row.age}</td><td>${row.salary}</td><td>${row.city}</td><td><button>X</button></td>`)
+      .join('');
+
+    table.append(tableBody);
+
+    table.addEventListener('click', this.onClick);
+
+    this.elem = table;
+  }
+
+  onClick (event) {
+    if (event.target.tagName === 'BUTTON') {
+      event.target.closest('TR').remove();
+    }
   }
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -3,5 +3,80 @@ import createElement from '../../assets/lib/create-element.js';
 export default class Carousel {
   constructor(slides) {
     this.slides = slides;
+    this.carousel = null;
+
+    this.render();
+  }
+
+  render() {
+    this.carousel = createElement(`<div class="carousel"></div>`);
+    let arrowLeft = createElement(`
+      <div class="carousel__arrow carousel__arrow_left">
+        <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+      </div>
+    `)
+    let arrowRight = createElement(`
+      <div class="carousel__arrow carousel__arrow_right">
+        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </div>
+    `)
+    let carouselInner = createElement(`<div class="carousel__inner"></div>`)
+
+
+    for (let slide of this.slides) {
+      carouselInner.append(createElement(`
+        <div class="carousel__slide" data-id="${slide.id}">
+          <img src="/assets/images/carousel/${slide.image}" class="carousel__img" alt="slide">
+          <div class="carousel__caption">
+            <span class="carousel__price">€${slide.price.toFixed(2)}</span>
+            <div class="carousel__title">${slide.name}</div>
+            <button type="button" class="carousel__button">
+              <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+            </button>
+          </div>
+        </div>
+      `)
+      );
+    }
+
+    this.carousel.append(arrowLeft);
+    this.carousel.append(arrowRight);
+    this.carousel.append(carouselInner);
+
+    let currentSlideNumber = 1;
+    let currentPosition = 0;
+
+    if (currentSlideNumber === 1) arrowLeft.style.display = "none";
+
+    arrowRight.addEventListener("click", function() {
+        carouselInner.style.transform = `translateX(-${currentPosition += document.querySelector('.carousel__inner').offsetWidth}px)`;
+        currentSlideNumber++;
+    })
+
+    arrowLeft.addEventListener("click", function() {
+        carouselInner.style.transform = `translateX(-${currentPosition -= document.querySelector('.carousel__inner').offsetWidth}px)`;
+        currentSlideNumber--;
+    })
+
+    this.carousel.addEventListener("click", function() {
+      arrowLeft.style.display = (currentSlideNumber === 1) ? "none" : "";
+      arrowRight.style.display = (currentSlideNumber === carouselInner.children.length) ? "none" : "";
+    })
+
+
+    this.carousel.addEventListener('click', function(event) {
+      if (event.target.closest('.carousel__button')) {
+        const e = new CustomEvent('product-add', {
+          detail: event.target.closest('.carousel__slide').dataset.id,
+          bubbles: true
+        });
+        this.dispatchEvent(e);
+      }
+    });
+
+  }
+
+  get elem() {
+    return this.carousel;
   }
 }
